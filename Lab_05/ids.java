38681fd2687433d0d153f8a6092070cca3700549
Sean White <seancrwhite@gmail.com>
2017-04-12 13:08:31 -0600

    Added Pcap files and some file reading

--- Lab_05/ids.java
+++ Lab_05/ids.java
@@ -1,5 +1,11 @@
 import java.io.File;
 
+import org.jnetpcap.Pcap;
+import org.jnetpcap.packet.JFlowMap;
+import org.jnetpcap.packet.JPacket;
+import org.jnetpcap.packet.JPacketHandler;
+import org.jnetpcap.protocol.tcpip.Tcp;
+
 public class ids {
     public static void main( String[] args ) {
         //File retreival
@@ -11,6 +17,19 @@ public class ids {
         } else {
             System.out.println( "Invalid input" );
             return;
+        }
+
+        final StringBuilder errorBuffer = new StringBuilder();
+        final Pcap pcap = Pcap.openOffline( traceFile, errorBuffer );
+
+        if(pcap == null){
+            System.err.println(errorBuffer);
+        }else{
+            pcap.loop(Pcap.LOOP_INFINITE, new JPacketHandler<StringBuilder>() {
+                final Tcp tcp = new Tcp();
+
+                
+            }, errorBuffer);
         }
     }
 }
