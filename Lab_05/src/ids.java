77f579448684ed530d889c57f84d933acb921b98
Sean White <seancrwhite@gmail.com>
2017-04-15 13:39:21 -0600

    Created Eclipse project and got jNetPcap working

---
+++ Lab_05/src/ids.java
@@ -0,0 +1,47 @@
+import org.jnetpcap.Pcap;
+import org.jnetpcap.packet.JPacket;
+import org.jnetpcap.packet.JPacketHandler;
+import org.jnetpcap.protocol.tcpip.Tcp;
+
+public class ids
+{
+	public static void main( String[] args )
+	{
+		// File retreival
+		String policyFile, traceFile;
+
+		if ( args.length >= 2 )
+		{
+			policyFile = args[0];
+			traceFile = args[1];
+		}
+		else
+		{
+			System.out.println( "Invalid input" );
+			return;
+		}
+
+		final StringBuilder errorBuffer = new StringBuilder();
+		final Pcap pcap = Pcap.openOffline( traceFile, errorBuffer );
+
+		if ( pcap == null )
+		{
+			System.err.println( errorBuffer );
+		}
+		else
+		{
+			pcap.loop( Pcap.LOOP_INFINITE, new JPacketHandler<StringBuilder>()
+			{
+				final Tcp tcp = new Tcp();
+
+				@Override
+				public void nextPacket( JPacket arg0, StringBuilder arg1 )
+				{
+					// TODO Auto-generated method stub
+
+				}
+
+			}, errorBuffer );
+		}
+	}
+}
