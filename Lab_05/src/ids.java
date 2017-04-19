07de73de3289d3f60aafe4e137743b5cb4a5225d
Tyler Bass <tylergbass@gmail.com>
2017-04-18 20:52:42 -0600

    Added a bunch of if statements

--- Lab_05/src/ids.java
+++ Lab_05/src/ids.java
@@ -8,13 +8,16 @@ import org.jnetpcap.Pcap;
 import org.jnetpcap.packet.JPacket;
 import org.jnetpcap.packet.JPacketHandler;
 import org.jnetpcap.packet.format.FormatUtils;
+import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Tcp;
 
 public class ids
 {
+	public static int matches, currentRule = 0;
+	
 	public static void main( String[] args ) throws FileNotFoundException
 	{
-		// File retreival
+		// File retrieval
 		String policyFile, traceFile;
 		
 		String host, name, type, proto, host_port, attacker_port, attacker;
@@ -64,15 +67,18 @@ public class ids
 		{
 			pcap.loop( Pcap.LOOP_INFINITE, new JPacketHandler<StringBuilder>()
 			{
+				final Ip4 ip4 = new Ip4();
 				final Tcp tcp = new Tcp();
 
 				@Override
 				public void nextPacket( JPacket packet, StringBuilder errorBuffer )
 				{
-					byte[] payload = packet.getHeader(tcp).getPayload();
-					String text = FormatUtils.hexdumpCombined(payload, 0, 0, false, true, false).replaceAll("\n *", "");
-//					System.out.println(text);
-					for (String regex : regexes) {
+					if ( packet.hasHeader( Tcp.ID ) && packet.hasHeader( ip4 ) ) {
+						
+						byte[] payload = packet.getHeader(tcp).getPayload();
+						String text = FormatUtils.hexdumpCombined(payload, 0, 0, false, true, false).replaceAll("\n *", "");
+						
+						String regex = regexes.get(currentRule);
 						boolean from = false;
 						if (regex.startsWith("from_host")) {
 							regex = regex.substring(11, regex.length() - 1);
@@ -81,11 +87,32 @@ public class ids
 						if (regex.startsWith("to_host")) {
 							regex = regex.substring(9, regex.length() - 1);
 						}
-						if (text.matches(".*" + regex + ".*")) {
-							System.out.println(text);
+						String source = FormatUtils.ip(ip4.source());
+						String destination = FormatUtils.ip(ip4.destination());
+						if (from && source.equals(host) || !from && destination.equals(host)) {
+							if ((from && destination.equals(attacker))
+							&& (!from && source.equals(attacker))
+							|| attacker.equals("any")) {
+								String sourcePort = Integer.toString(tcp.source());
+								String destinationPort = Integer.toString(tcp.destination());
+								String hostPort = from ? sourcePort : destinationPort;
+								String attackerPort = from ? destinationPort : sourcePort;
+								if (host_port.equals("any") || host_port.equals(hostPort)) {
+									if (attacker_port.equals("any")
+									|| attacker_port.equals(attackerPort)) {
+										if (text.matches(".*" + regex + ".*")) {
+											System.out.println(text);
+											currentRule++;
+											if (currentRule == regexes.size()) {
+												currentRule = 0;
+											}
+											
+										}
+									}
+								}
+							}
 						}
 					}
-
 				}
 
 			}, errorBuffer );
